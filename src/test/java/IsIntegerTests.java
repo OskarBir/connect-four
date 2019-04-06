1f6ac3b0b3d61106a10922e2f8a3cef1efaaa007
Oskar <OskarBir>
2019-04-06 22:38:03 +0200

    added more test cases

--- src/test/java/IsIntegerTests.java
+++ src/test/java/IsIntegerTests.java
@@ -9,8 +9,16 @@ public class IsIntegerTests {
             "2,true",
             "3.0, false",
             "string,false",
+            "-10.0, false",
             "-1, true",
-            ",false"
+            "3.14F, false",
+            "-3.14F, false",
+            "0, true",
+            ",false",
+            "-1000, true",
+            "true, false",
+            "false, false",
+            "null, false",
     })
     public void isIntegerTest(String input,Boolean expected) {
         assertEquals(expected, ConnectFour.isInteger(input));
