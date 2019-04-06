1268fbeaae4d59aaa2834a591cef44cb07263d41
Oskar <OskarBir>
2019-04-06 22:31:58 +0200

    fixed parametrized tests and now on junit5

--- src/test/java/IsIntegerTests.java
+++ src/test/java/IsIntegerTests.java
@@ -1,33 +1,18 @@
-import static org.junit.Assert.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
 
-import java.util.Arrays;
-import java.util.Collection;
-
-import org.junit.Test;
-import org.junit.runner.RunWith;
-import org.junit.runners.Parameterized;
-import org.junit.runners.Parameterized.Parameters;
-
-@RunWith(Parameterized.class)
 public class IsIntegerTests {
-    @Parameters
-    public static Collection<Object[]> data() {
-        return Arrays.asList(new Object[][]{
-                {"1", true}, {"2", true}, {"2.0", false}, {"3.0", false}, {"string", false}, {"-1", true}, {"", false}
-        });
-    }
-
-    private String fInput;
-
-    private Boolean fExpected;
-
-    public IsIntegerTests(String input, Boolean expected) {
-        this.fInput = input;
-        this.fExpected = expected;
-    }
-
-    @Test
-    public void test() {
-        assertEquals(fExpected, ConnectFour.isInteger(fInput));
+    @ParameterizedTest
+    @CsvSource({
+            "1,true",
+            "2,true",
+            "3.0, false",
+            "string,false",
+            "-1, true",
+            ",false"
+    })
+    public void isIntegerTest(String input,Boolean expected) {
+        assertEquals(expected, ConnectFour.isInteger(input));
     }
 }
--- src/test/java/IsWonTests.java
+++ src/test/java/IsWonTests.java
@@ -1,15 +1,12 @@
-import junitparams.FileParameters;
-import junitparams.JUnitParamsRunner;
-import org.junit.Test;
-import org.junit.runner.RunWith;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvFileSource;
 
-import static org.junit.Assert.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
-@RunWith(JUnitParamsRunner.class)
 public class IsWonTests {
 
-    @Test
-    @FileParameters("src/test/test.csv")
+    @ParameterizedTest
+    @CsvFileSource(resources = "/test.csv")
     public void isIntegerParameterFromFileTest(int x1, int y1, char c1, int x2, int y2, char c2, int x3, int y3, char c3, int x4, int y4, char c4, boolean state) {
         ConnectFour connectFour = new ConnectFour();
         connectFour.board[x1][y1] = c1;
